dc878b1b199ca48ef4221e1822abafb14801b9ba
Ds-Isaias <isaiasdesouzadocarmo07@gmail.com>
2025-07-05 03:52:42 +0000

    Limpeza da pasta ds_one_app e reorganização da estrutura Flutter

---
+++ android/app/src/main/kotlin/com/example/ds_one/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.ds_one import 
+io.flutter.embedding.android.FlutterActivity 
+class MainActivity: FlutterActivity() {
+}
+
+
+
--- ds_one_app/android/app/src/main/kotlin/com/example/ds_one_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ds_one_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
